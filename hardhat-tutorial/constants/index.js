8e4abee2d5681a2eeb102aa531ea20f8d4262726
sottiz <alexandre.pataut@icloud.com>
2022-04-21 15:59:36 +0200

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xC260d45700b30454b5D6BCE2cac5FF3C2325084C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
